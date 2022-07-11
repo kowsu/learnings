eb1eec6fcddb5845d3bf61bb0c1e8f1f814d1d8c
kowsu <ramanjineyulu.kld@gmail.com>
2022-07-11 10:52:48 +0530

    first commit

---
+++ spring-boot-web-hello-world-app/src/main/java/io/kowsu/SpringBootWebHelloWorldAppApplication.java
@@ -0,0 +1,13 @@
+package io.kowsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebHelloWorldAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebHelloWorldAppApplication.class, args);
+	}
+
+}
